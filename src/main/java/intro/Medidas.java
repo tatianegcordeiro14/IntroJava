34f996449c9416c627ce76ebe98f315402a30894
ferna <tatianegcordeiro.14@gmail.com>
2021-06-16 22:18:58 -0300

    Incluí o "Boa noite!!" no começo da impressão de texto no console

--- src/main/java/intro/Medidas.java
+++ src/main/java/intro/Medidas.java
@@ -11,6 +11,7 @@ public class Medidas {
     public static void main(String[] args){
         System.out.println("Cálculo de áreas.");
         System.out.println("Bom dia!");
+        System.out.println("Boa noite!!");
 
     }
 
